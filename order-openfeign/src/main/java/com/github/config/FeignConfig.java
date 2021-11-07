15b3eb72fa6aba2f26d47f85db90ec347b2aaf5a
cymin <cymin2017@163.com>
2021-11-07 14:25:05 +0800

    通过配置文件，修改feign的日志级别，配置调用product-service的接口日志级别为BASIC

--- order-openfeign/src/main/java/com/github/config/FeignConfig.java
+++ order-openfeign/src/main/java/com/github/config/FeignConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 /**
  * 两种配置方式
  * 全局配置：使用@Configuration，作用于所有的服务提供者，会打印所有调用提供者rest接口的日志
- * 局部配置：不使用@Configuration，作用于某一个服务提供者
+ * 局部配置：2种方法：1.不使用@Configuration，作用于某一个服务提供者; 2.使用application.yml配置文件
  * @author : cymin
  * @date : 2021.11.07 下午12:37
  */
