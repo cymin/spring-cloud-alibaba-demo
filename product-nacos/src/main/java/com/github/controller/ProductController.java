0078a04e460f725ac35a78c0dca6cf9ac155b1e2
cymin <cymin2017@163.com>
2021-11-07 12:32:09 +0800

    新增module: product-nacos

---
+++ product-nacos/src/main/java/com/github/controller/ProductController.java
@@ -0,0 +1,24 @@
+package com.github.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by cymin on 2021/11/4.
+ */
+@RestController
+@RequestMapping("product")
+public class ProductController {
+
+    @Value("${server.port}")
+    String port;
+
+    @GetMapping("/get/{id}")
+    public String get(@PathVariable String id) {
+        System.out.println("查询到商品id：" + id);
+        return "查询到商品id：" + id + " port=" + port;
+    }
+}
