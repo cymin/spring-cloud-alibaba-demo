1a58503770380facd4fe3c704092bf1be837e978
cymin <cymin2017@163.com>
2021-11-08 17:51:08 +0800

    测试线程流控

--- order-sentinel/src/main/java/com/github/controller/OrderController.java
+++ order-sentinel/src/main/java/com/github/controller/OrderController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by cymin on 2021/11/4.
  * 默认一旦spring应用重启之后，在sentinel控制台中配置的信息都会丢失
@@ -29,4 +31,18 @@ public class OrderController {
     public String addBlockHandler(BlockException e) {
         return "被流控";
     }
+
+    /**
+     * 线程流控
+     * 同一时间只能有一个线程访问，等这个线程处理完了，才可以处理另一个线程
+     * @return
+     */
+    @GetMapping("/thread")
+    @SentinelResource(value = "thread", blockHandler = "addBlockHandler")
+    public String threadFlow() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(3);
+        return "success";
+    }
+
+
 }
