f678c5e6b34380e929aef2d7a16d0cacfb348add
cymin <cymin2017@163.com>
2021-11-08 16:29:57 +0800

    order-sentinel:增加被流控的处理方法

--- order-sentinel/src/main/java/com/github/controller/OrderController.java
+++ order-sentinel/src/main/java/com/github/controller/OrderController.java
@@ -1,5 +1,7 @@
 package com.github.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,13 +9,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by cymin on 2021/11/4.
+ * 默认一旦spring应用重启之后，在sentinel控制台中配置的信息都会丢失
  */
 @RestController
 @RequestMapping("order")
 public class OrderController {
 
+    /**
+     * 流控
+     * 注意value的值
+     * @return
+     */
     @GetMapping("/add")
+    @SentinelResource(value = "add", blockHandler = "addBlockHandler")
     public String add() {
         return "success";
+    }
+
+    public String addBlockHandler(BlockException e) {
+        return "被流控";
     }
 }
